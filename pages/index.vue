038997f4718307167dc1f70f7f209edda9ad2ef5
Jorge isacc <jorge@wdinamo.com>
2020-05-04 22:20:46 -0500

    First commit with sass and tailwind config

---
+++ pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container max-w-full bg-atoom-gp">
+    <div>
+      <logo />
+      <h1 class="title my-4 text-yellow-300">
+        atoomi-xt
+      </h1>
+      <h2 class="subtitle">
+        atoomik website developed with vue and nuxt
+      </h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
