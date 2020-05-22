b0e4c0808dd7355b702dc8f1bee1d00f34d76a97
Jorge isacc <jorge@wdinamo.com>
2020-05-22 05:52:39 -0500

    Creando estructura async con md

---
+++ content/ux.md
@@ -0,0 +1,5 @@
+---
+title: User Experience
+description: Algo super mega re quete cool
+image: /images/user-flow.svg
+---
--- pages/servicios.vue
+++ pages/servicios.vue
@@ -14,43 +14,30 @@
         </p>
       </div>
       <div class="my-6">
-        <div class="flex justify-center w-full">
-          <services
-            :imgSrc="require('~/assets/images/svg-draws/portfolio.svg')"
-            :title="'Desarrollo Web'"
-            @showText="serviceDesc()"
-          />
-          <services
-            :imgSrc="require('~/assets/images/svg-draws/user-flow.svg')"
-            :title="'User Experience'"
-            @showText="serviceDesc()"
-          /><services
-            :imgSrc="require('~/assets/images/svg-draws/experience-design.svg')"
-            :title="'Desarrollo Web'"
-            @showText="serviceDesc()"
-          />
-        </div>
-        <div class="w-1/2 mx-auto my-6">
-          <div v-if="showDescription">
-            <p class="slogan p-4 text-6xl bg-white">
-              Desarrollo web
-            </p>
-            <p class="text-white">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+        <template v-for="(content, i) in allServices">
+          <div :key="i" class="flex justify-center w-full">
+            <services
+              :imgSrc="content.image"
+              :title="'content.title'"
+              @showText="serviceDesc()"
+            />
           </div>
-          <div v-else class=" w-full h-64 bg-atoom-ylw">
-            <p class="text-white text-2xl">
-              Selecciona una imagen para saber mas
-            </p>
+          <div :key="i" class="w-1/2 mx-auto my-6">
+            <div v-if="showDescription">
+              <p class="slogan p-4 text-6xl bg-white">
+                {{ content.title }}
+              </p>
+              <p class="text-white">
+                {{ content.description }}
+              </p>
+            </div>
+            <div v-else class=" w-full h-64 bg-atoom-ylw">
+              <p class="text-white text-2xl">
+                Selecciona una imagen para saber mas
+              </p>
+            </div>
           </div>
-        </div>
+        </template>
       </div>
     </div>
   </section>
@@ -60,6 +47,19 @@
 import Services from "../components/HoverCards.vue";
 export default {
   name: "Servicios",
+  async asyncData() {
+    const req = require.context("~/content/", true, /\.md$/, "lazy");
+    const exp = /([\w-]+)\.md$/;
+
+    const allServices = await Promise.all(
+      req.keys().map(async key => {
+        const [,] = key.match(exp) || [];
+        const { title, description, image } = await req(key);
+        return { title, description, image };
+      })
+    );
+    return { allServices };
+  },
   data: () => ({
     showDescription: false
   }),
