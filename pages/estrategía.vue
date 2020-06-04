612443dd3657fa1616904d6bcac836f3dbb845e2
Jorge isacc <jorge@wdinamo.com>
2020-06-04 03:51:56 -0500

    Mejora de diseño en hero y animación de hero

--- pages/estrategía.vue
+++ pages/estrategía.vue
@@ -1,52 +1,58 @@
 <template>
   <section class="bg-atoom-bl">
-    <div class="contain">
+    <div class="w-full">
       <div
-        class="flex flex-col justify-around min-h-adjust lg:flex-row lg:items-center"
+        class="flex flex-col justify-between min-h-adjust lg:flex-row lg:items-center"
       >
-        <div class="my-8 lg:w-1/2">
-          <img
-            class="max-w-xl w-full mx-auto"
-            src="../assets/images/svg-draws/planning.svg"
-            alt="services asset"
-          />
-        </div>
         <div class="w-full my-8 overflow-hidden lg:w-1/2">
-          <span class="entrance text-right">
+          <div class="entrance px-8 text-center lg:text-right text-white">
             <p class="slogan text-4xl lg:text-xxl">
               Estrategia
             </p>
             <p class="text-lg">
               O sobre como vamos a crear un proyecto ganador.
             </p>
-          </span>
+          </div>
           <span class="flex justify-center">
             <fa-icon class="down-arrow" :icon="['fad', 'angle-double-down']" />
           </span>
         </div>
+        <div class="flex p-8 bg-white min-h-partial lg:w-1/2 lg:min-h-adjust">
+          <img
+            class="max-w-xl w-full mx-auto"
+            src="../assets/images/svg-draws/product-plan.svg"
+            alt="services asset"
+          />
+        </div>
       </div>
-      <div>
-        <p>
-          ¿Mis servicios? Desarrollar páginas web, sin embargo el verdadero
-          valor de un desarrollo que queda en mis manos, son los pequeños
-          detalles que pueden convertirlo en una de las mejores inversiones que
-          realices, que tengan lo necesario para satisfacer tus necesidades
-          actuales, pero sin que esto signifique que tu sitio no pueda crecer
-          contigo en el futuro y que puedas contar con él en todo momento.
-        </p>
-        <br />
-        <p>
-          Aquí puedo explicarte más específicamente de aquello que le da valor a
-          mis proyectos.
-        </p>
-      </div>
+      <!--Page content-->
+      <div class="w-full p-8 bg-white">
+        <div class="contain">
+          <div>
+            <p>
+              ¿Mis servicios? Desarrollar páginas web, sin embargo el verdadero
+              valor de un desarrollo que queda en mis manos, son los pequeños
+              detalles que pueden convertirlo en una de las mejores inversiones
+              que realices, que tengan lo necesario para satisfacer tus
+              necesidades actuales, pero sin que esto signifique que tu sitio no
+              pueda crecer contigo en el futuro y que puedas contar con él en
+              todo momento.
+            </p>
+            <br />
+            <p>
+              Aquí puedo explicarte más específicamente de aquello que le da
+              valor a mis proyectos.
+            </p>
+          </div>
 
-      <div class="flex">
-        <button
-          class="mx-auto my-8 px-4 py-2 text-2xl text-atoom-gp border-4 border-atoom-gp rounded-lg transition-all duration-300 hover:text-white hover:bg-atoom-gp"
-        >
-          Comencemos
-        </button>
+          <div class="flex">
+            <button
+              class="mx-auto my-8 px-4 py-2 text-2xl text-atoom-gp border-4 border-atoom-gp rounded-lg transition-all duration-300 hover:text-white hover:bg-atoom-gp"
+            >
+              Comencemos
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </section>
--- pages/servicios.vue
+++ pages/servicios.vue
@@ -1,72 +1,87 @@
 <template>
-  <section>
-    <div class="contain">
-      <div
-        class="flex flex-col justify-around min-h-adjust lg:flex-row lg:items-center"
-      >
-        <div class="w-full my-8 overflow-hidden lg:w-1/2">
-          <span class="entrance">
-            <p class="slogan text-4xl lg:text-xxl">
-              Servicios
-            </p>
-            <p class="text-lg">
-              Más que el desarrollo de un sitio web.
-            </p>
-          </span>
-          <span class="flex justify-center">
-            <fa-icon class="down-arrow" :icon="['fad', 'angle-double-down']" />
-          </span>
-        </div>
-        <div class="my-8 lg:w-1/2">
-          <img
-            class="max-w-xl w-full mx-auto"
-            src="../assets/images/svg-draws/feeling-proud.svg"
-            alt="services asset"
-          />
-        </div>
-      </div>
-      <div>
-        <p>
-          ¿Mis servicios? Desarrollar páginas web, sin embargo el verdadero
-          valor de un desarrollo que queda en mis manos, son los pequeños
-          detalles que pueden convertirlo en una de las mejores inversiones que
-          realices, que tengan lo necesario para satisfacer tus necesidades
-          actuales, pero sin que esto signifique que tu sitio no pueda crecer
-          contigo en el futuro y que puedas contar con él en todo momento.
-        </p>
-        <br />
-        <p>
-          Aquí puedo explicarte más específicamente de aquello que le da valor a
-          mis proyectos.
-        </p>
-      </div>
-      <div class="my-6 flex flex-col lg:justify-around lg:flex-row">
-        <vue-carousel
-          :per-page-custom="[
-            [0, 1],
-            [640, 2],
-            [1280, 4]
-          ]"
-          paginationColor="#e4c85e"
-          paginationActiveColor="#2f2f2f"
-          :navigationEnabled="true"
-          :loop="true"
+  <section class="bg-atoom-ylw">
+    <div>
+      <div class="w-full">
+        <div
+          class="flex flex-col justify-between min-h-adjust lg:flex-row lg:items-center"
         >
-          <vue-slide v-for="(service, i) in allServices" :key="i" class="p-4">
-            <services
-              :imgSrc="service.image"
-              :title="service.name"
-              :description="service.desc"
+          <div class="w-full my-8 overflow-hidden lg:w-1/2">
+            <div class="entrance px-8 text-center lg:text-right">
+              <p class="slogan text-4xl lg:text-xxl">
+                Servicios
+              </p>
+              <p class="text-lg">
+                Más que el desarrollo de un sitio web.
+              </p>
+            </div>
+            <span class="flex justify-center">
+              <fa-icon
+                class="down-arrow"
+                :icon="['fad', 'angle-double-down']"
+              />
+            </span>
+          </div>
+          <div class="flex p-8 bg-white min-h-partial lg:w-1/2 lg:min-h-adjust">
+            <img
+              class="max-w-xl w-full mx-auto"
+              src="../assets/images/svg-draws/feeling-proud.svg"
+              alt="services asset"
             />
-          </vue-slide>
-        </vue-carousel>
+          </div>
+        </div>
       </div>
-      <div class="flex">
-        <button
-          class="mx-auto my-8 px-4 py-2 text-2xl text-atoom-gp border-4 border-atoom-gp rounded-lg transition-all duration-300 hover:text-white hover:bg-atoom-gp"
-        >
-          Comencemos
-        </button>
+      <!--Page content-->
+      <div class="w-full p-8 bg-white">
+        <div class="contain">
+          <div>
+            <p>
+              ¿Mis servicios? Desarrollar páginas web, sin embargo el verdadero
+              valor de un desarrollo que queda en mis manos, son los pequeños
+              detalles que pueden convertirlo en una de las mejores inversiones
+              que realices, que tengan lo necesario para satisfacer tus
+              necesidades actuales, pero sin que esto signifique que tu sitio no
+              pueda crecer contigo en el futuro y que puedas contar con él en
+              todo momento.
+            </p>
+            <br />
+            <p>
+              Aquí puedo explicarte más específicamente de aquello que le da
+              valor a mis proyectos.
+            </p>
+          </div>
+          <div class="my-6 flex flex-col lg:justify-around lg:flex-row">
+            <vue-carousel
+              :per-page-custom="[
+                [0, 1],
+                [640, 2],
+                [1280, 4]
+              ]"
+              paginationColor="#e4c85e"
+              paginationActiveColor="#2f2f2f"
+              :navigationEnabled="true"
+              :loop="true"
+            >
+              <vue-slide
+                v-for="(service, i) in allServices"
+                :key="i"
+                class="p-4"
+              >
+                <services
+                  :imgSrc="service.image"
+                  :title="service.name"
+                  :description="service.desc"
+                />
+              </vue-slide>
+            </vue-carousel>
+          </div>
+          <div class="flex">
+            <button
+              class="mx-auto my-8 px-4 py-2 text-2xl text-atoom-gp border-4 border-atoom-gp rounded-lg transition-all duration-300 hover:text-white hover:bg-atoom-gp"
+            >
+              Comencemos
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   </section>
