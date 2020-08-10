165893eb7c8ee26b8293b7fa1a74f0c6203b177b
Jorge isacc <jorge@wdinamo.com>
2020-08-09 22:50:35 -0500

    refactoring about me to the grad design

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,17 +1,17 @@
 <template>
-  <section class="bg-black">
-    <div class="relative overflow-hidden w-full h-64">
+  <section class="sln-gp-grad">
+    <!--<div class="relative overflow-hidden w-full h-64">
       <img
         class="absolute w-full h-full object-cover"
         src="../assets/images/back.jpg"
         alt="persona de espaldas"
       />
-    </div>
+    </div>-->
     <div class="contain flex flex-col items-start min-h-adjust lg:flex-row">
-      <div class="hidden lg:block lg:-mt-16 lg:h-full lg:w-1/4">
-        <div class="relative overflow-hidden h-128">
+      <div class="hidden lg:block lg:h-full lg:w-1/4">
+        <div class="relative overflow-hidden h-80 lg:mt-20 rounded-lg shadow-transp">
           <img
-            class="object-cover border-atoom-ylw border-r-4"
+            class="object-cover rounded-lg shadow-lg"
             src="../assets/images/lateral1.jpg"
             alt="profile picture"
           />
@@ -24,7 +24,7 @@
         </div>
       </div>
       <div class="w-full lg:w-3/4 text-white text-center lg:text-left lg:mt-20 lg:pl-20">
-        <p class="text-xxl font-thin text-black -mx-8 bg-atoom-ylw lg:pl-4 lg:mx-0">About me</p>
+        <p class="text-xxl font-thin text-atoom-semiGray -mx-8 lg:pl-4 lg:mx-0">About me</p>
         <p class="mt-8 px-8 lg:mt-24 lg:pl-16">
           Isacc Núñez, apasionado de las tecnologias y el diseño web, egresado
           de BEDU Tech donde inicie mi formación como desarrollador web.
