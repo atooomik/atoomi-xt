a6cbb96522fa412293ec21b8071b7faf28dd0f41
Isacc Nuñez <jorge@wdinamo.com>
2020-08-18 03:23:02 -0500

    New card component for services/desktop

---
+++ components/HoverSideCards.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="main" class="grid grid-cols-2 overflow-hidden">
+    <div class="main-traslate-r">
+      <img class="w-48 self-center" :src="imgAsset" alt="" />
+      <p v-text="title" class="text-white"></p>
+    </div>
+    <div class="main-traslate-l p-1 bg-atoom-semiGray rounded-lg">
+      <p v-text="resume" class="text-white"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HoverSideCards",
+  props: {
+    imgAsset: {
+      type: String,
+      default: ""
+    },
+    title: {
+      type: String,
+      default: "Malaka"
+    },
+    resume: {
+      type: String,
+      default: "Malaka"
+    }
+  }
+};
+</script>
--- pages/servicios.vue
+++ pages/servicios.vue
@@ -2,14 +2,21 @@
   <section class="ylw-bl-grad">
     <div>
       <div class="w-full">
-        <div class="h-screen flex flex-col justify-between lg:flex-row lg:items-center">
+        <div
+          class="h-screen flex flex-col justify-between lg:flex-row lg:items-center"
+        >
           <div class="w-full mt-24 overflow-hidden lg:w-1/2 lg:my-8">
             <div class="entrance px-8 text-center lg:text-right">
               <p class="text-4xl text-white font-thin lg:text-xxl">Servicios</p>
-              <p class="text-lg text-white">Más que el desarrollo de un sitio web.</p>
+              <p class="text-lg text-white">
+                Más que el desarrollo de un sitio web.
+              </p>
             </div>
             <span class="flex justify-end mr-40">
-              <fa-icon class="down-arrow" :icon="['fad', 'angle-double-down']" />
+              <fa-icon
+                class="down-arrow"
+                :icon="['fad', 'angle-double-down']"
+              />
             </span>
           </div>
           <div class="flex p-8 min-h-partial lg:w-1/2 lg:min-h-adjust">
@@ -35,15 +42,19 @@
               todo momento.
             </p>
             <p class="text-white">
-              Aquí puedo explicarte más específicamente de aquello que le da
-              valor a mis proyectos.
+              Cuando me contratas para la creación de tu sitio web, este es el
+              conjunto real de servicios y caracteristicas que estaran
+              implementadas en tu sitio web:
             </p>
           </div>
-          <div v-if="isMobile" class="my-6 flex flex-col lg:justify-around lg:flex-row">
+          <div
+            v-if="isMobile"
+            class="my-6 flex flex-col lg:justify-around lg:flex-row"
+          >
             <vue-carousel
               :per-page-custom="[
                 [0, 1],
-                [640, 2],
+                [640, 2]
               ]"
               paginationColor="#e4c85e"
               paginationActiveColor="#ffffff"
@@ -52,14 +63,31 @@
               autoplay="true"
             >
               <vue-slide v-for="(item, i) in services" :key="i" class="p-4">
-                <services :imgSrc="`images/svg-draws/${item.image}`" :title="item.name" :description="item.desc" />
+                <services
+                  :imgSrc="`images/svg-draws/${item.image}`"
+                  :title="item.name"
+                  :description="item.desc"
+                />
               </vue-slide>
             </vue-carousel>
+          </div>
+          <div v-else>
+            <div class="grid grid-cols-2 gap-8 py-8">
+              <hover-sides
+                v-for="(card, i) in services"
+                :key="i"
+                :imgAsset="`images/svg-draws/${card.image}`"
+                :title="card.name"
+                :resume="card.desc"
+              />
+            </div>
           </div>
           <div class="flex">
             <button
               class="mx-auto my-8 px-4 py-2 text-2xl text-white rounded-pill bg-atoom-ylw shadow-lg"
-            >Comencemos</button>
+            >
+              Comencemos
+            </button>
           </div>
         </div>
       </div>
@@ -72,23 +100,25 @@ import VueCarousel from "vue-carousel/src/Carousel.vue";
 import VueSlide from "vue-carousel/src/Slide.vue";
 
 import Services from "../components/HoverCards.vue";
+import HoverSides from "../components/HoverSideCards.vue";
 import { services } from "../assets/data/servicios.json";
 
 export default {
   name: "Servicios",
   asyncData() {
     return {
-      services,
+      services
     };
   },
   data: () => ({
     services: [],
-    isMobile: false,
+    isMobile: false
   }),
   components: {
     Services,
     VueCarousel,
     VueSlide,
+    HoverSides
   },
   mounted() {
     this.mobileScreen();
@@ -98,7 +128,7 @@ export default {
     mobileScreen() {
       this.isMobile = window.innerWidth < 1024;
       console.log(this.isMobile);
-    },
-  },
+    }
+  }
 };
 </script>
