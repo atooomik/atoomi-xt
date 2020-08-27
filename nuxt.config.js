7dcf20d6b61a00cc6f818ee89cc401ee38d75048
Isacc Nuñez <jorge@wdinamo.com>
2020-08-26 21:18:28 -0500

    Recovering initial-scale on mobile

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const head = {
   meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
     {
       hid: "keywords",
       name: "keywords",
