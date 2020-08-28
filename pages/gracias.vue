5d7d36e75570e8758358e42c0aa3433cc448ffde
Isacc Nuñez <jorge@wdinamo.com>
2020-08-28 01:08:10 -0500

    Ajustando pagina de gracias para desktop

--- pages/gracias.vue
+++ pages/gracias.vue
@@ -8,12 +8,12 @@
           ¡Un michi ha interceptado tus datos!
         </p>
         <div
-          class="flex flex-col mx-auto mt-8 p-4 rounded-lg lg:flex-row lg:mt-8 lg:max-w-2xl xl:max-w-4xl"
+          class="flex flex-col mx-auto mt-8 p-4 rounded-lg lg:mt-8 lg:max-w-2xl xl:max-w-4xl"
         >
-          <div class="w-48 mx-auto md:w-64 lg:w-1/2 lg:flex lg:items-center">
+          <div class="w-48 mx-auto md:w-64 lg:w-1/3 lg:flex lg:items-center">
             <img src="../assets/images/svg-draws/thankfull-cat.svg" alt />
           </div>
-          <div class="lg:flex lg:items-end lg:w-1/2">
+          <div class="">
             <p class="mt-12 text-lg text-white text-center lg:text-3xl">
               Cuando lo atrape y me los entregue, me pondré en contacto contigo.
             </p>
