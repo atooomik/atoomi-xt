f9841478372472d17e05c2305e8a723f99623e8b
Isacc Nuñez <jorge@wdinamo.com>
2020-09-21 15:42:59 -0500

    Purging keys on ModalCards

--- components/ModalCards.vue
+++ components/ModalCards.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="modal-card" @click="showModal()">
-      <img :src="imgSrc" alt="" />
+      <img :src="imgSrc" alt />
       <p v-text="proyectTitle" class="text-white px-4 py-2"></p>
     </div>
     <!--Modal con detalles del proyecto-->
@@ -10,21 +10,14 @@
         <div
           class="fixed top-0 right-0 bottom-0 left-0 flex items-center w-full h-screen p-4 bg-atoom-dkOverlay"
         >
-          <div
-            class="relative max-w-4xl mx-auto lg:mt-16 rounded-lg cursor-default"
-          >
+          <div class="relative max-w-4xl mx-auto lg:mt-16 rounded-lg cursor-default">
             <button
               class="absolute z-10 right-0 bottom-0 left-0 mx-auto -mb-4 px-1 pt-1 bg-white rounded-full"
               @click="showModal()"
             >
-              <fa-icon
-                class=" text-black text-2xl"
-                :icon="['fad', 'times-circle']"
-              />
+              <fa-icon class="text-black text-2xl" :icon="['fad', 'times-circle']" />
             </button>
-            <div
-              class="w-full h-80 rounded-lg lg:h-84 bg-white overflow-y-scroll"
-            >
+            <div class="w-full h-80 rounded-lg lg:h-84 bg-white overflow-y-scroll">
               <div class="w-full">
                 <slot name="carousel"></slot>
               </div>
@@ -34,24 +27,16 @@
                   <p v-text="proyectTitle"></p>
                 </div>
                 <div class="mb-4">
-                  <p class="font-normal">
-                    Participación:
-                  </p>
+                  <p class="font-normal">Participación:</p>
                   <p v-text="proyectTasks"></p>
                 </div>
                 <div class="mb-4">
-                  <p class="font-normal">
-                    Retos:
-                  </p>
+                  <p class="font-normal">Retos:</p>
                   <p v-text="proyectChallenges"></p>
                 </div>
                 <div class="mb-4">
-                  <p class="font-normal">
-                    Principales tecnologias utilizadas en el proyecto:
-                  </p>
-                  <div
-                    class="grid grid-cols-4 justify-items-center text-3xl text-atoom-semiGray"
-                  >
+                  <p class="font-normal">Principales tecnologias utilizadas en el proyecto:</p>
+                  <div class="grid grid-cols-4 justify-items-center text-3xl text-atoom-semiGray">
                     <slot name="tecnologies"></slot>
                   </div>
                 </div>
@@ -62,24 +47,16 @@
                   <p v-text="proyectTitle"></p>
                 </div>
                 <div class="mb-4">
-                  <p class="font-normal">
-                    My participation:
-                  </p>
+                  <p class="font-normal">My participation:</p>
                   <p v-text="proyectTasks"></p>
                 </div>
                 <div class="mb-4">
-                  <p class="font-normal">
-                    The challenges:
-                  </p>
+                  <p class="font-normal">The challenges:</p>
                   <p v-text="proyectChallenges"></p>
                 </div>
                 <div class="mb-4">
-                  <p class="font-normal">
-                    Main technologies used on the project:
-                  </p>
-                  <div
-                    class="grid grid-cols-4 justify-items-center text-3xl text-atoom-semiGray"
-                  >
+                  <p class="font-normal">Main technologies used on the project:</p>
+                  <div class="grid grid-cols-4 justify-items-center text-3xl text-atoom-semiGray">
                     <slot name="tecnologies"></slot>
                   </div>
                 </div>
@@ -98,33 +75,33 @@ export default {
   props: {
     cardLang: {
       type: String,
-      default: "Español"
+      default: "Español",
     },
     imgSrc: {
       type: String,
-      default: ""
+      default: "",
     },
     proyectTitle: {
       type: String,
-      default: ""
+      default: "",
     },
     proyectTasks: {
       type: String,
-      default: ""
+      default: "",
     },
     proyectChallenges: {
       type: String,
-      default: ""
-    }
+      default: "",
+    },
   },
   data: () => ({
-    openOverlay: false
+    openOverlay: false,
   }),
   methods: {
     showModal() {
       this.openOverlay = !this.openOverlay;
-      console.log(this.openModal);
-    }
-  }
+      console.log(this.openOverlay);
+    },
+  },
 };
 </script>
--- pages/portafolio.vue
+++ pages/portafolio.vue
@@ -7,21 +7,17 @@
             @click="selectEsp"
             class="px-4 py-2"
             :class="[{ 'active-link': esp }, { 'non-active-link': eng }]"
-          >
-            ES
-          </button>
+          >ES</button>
           <span>|</span>
           <button
             @click="selectEng"
             class="px-4 py-2"
             :class="[{ 'active-link': eng }, { 'non-active-link': esp }]"
-          >
-            EN
-          </button>
+          >EN</button>
         </div>
         <div class="w-4/5 pt-32 mx-auto md:w-2/5 lg:w-1/4 lg:pt-0 lg:mx-0">
           <div
-            class="relative traslate-shadow-entrance h-72 mx-auto overflow-hidden rounded-lg  lg:mt-40"
+            class="relative traslate-shadow-entrance h-72 mx-auto overflow-hidden rounded-lg lg:mt-40"
           >
             <img
               class="absolute w-full h-full object-cover"
@@ -31,14 +27,10 @@
           </div>
         </div>
         <div v-if="esp" class="lg:w-3/4">
-          <div
-            class="opacity-entrance relative pb-8 lg:pl-20 lg:mt-32 text-white text-left"
-          >
+          <div class="opacity-entrance relative pb-8 lg:pl-20 lg:mt-32 text-white text-left">
             <p
-              class="mt-4 text-4xl text-center lg:text-left lg:font-thin lg:text-xxl lg:text-atoom-semiGray lg:pl-4  "
-            >
-              Acerca de mi
-            </p>
+              class="mt-4 text-4xl text-center lg:text-left lg:font-thin lg:text-xxl lg:text-atoom-semiGray lg:pl-4"
+            >Acerca de mi</p>
             <div class="lg:pr-8 lg:pl-16 lg:mt-20">
               <p class="mb-4">
                 Isacc Núñez, apasionado por la tecnología y el diseño web,
@@ -63,12 +55,8 @@
           <div class="opacity-entrance opacity-entrance--second">
             <p
               class="pb-8 text-2xl text-center text-white lg:text-left lg:text-3xl"
-            >
-              Proyectos en los que he trabajado:
-            </p>
-            <div
-              class="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-8 justify-items-center"
-            >
+            >Proyectos en los que he trabajado:</p>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-8 justify-items-center">
               <modalcards
                 v-for="(item, i) in portafolio"
                 :key="i"
@@ -81,41 +69,22 @@
               >
                 <template v-slot:carousel>
                   <vue-carousel :per-page="1" :loop="true" :autoplay="true">
-                    <vue-slide
-                      v-for="(value, index) in item.galery"
-                      :key="'value' + index"
-                    >
-                      <img :src="`images/portafolio/${value.img}`" alt="" />
+                    <vue-slide v-for="(value, index) in item.galery" :key="index">
+                      <img :src="`images/portafolio/${value.img}`" alt />
                     </vue-slide>
                   </vue-carousel>
                 </template>
                 <template v-slot:tecnologies>
-                  <div
-                    v-for="(value, index) in item.tecnologies"
-                    :key="'value' + index"
-                  >
+                  <div v-for="(value, index) in item.tecnologies" :key="index">
                     <fa-icon
                       class="m-4 mb-2 text-atoom-dkOverlay"
                       :icon="['fab', `${value.icon}`]"
                     />
-                    <p
-                      v-text="value.name"
-                      class="text-xs text-center text-black"
-                    ></p>
+                    <p v-text="value.name" class="text-xs text-center text-black"></p>
                   </div>
-                  <div
-                    v-for="(value, index) in item.specialItems"
-                    :key="'value' + index"
-                  >
-                    <img
-                      class="w-8 h-8 m-4 mb-2"
-                      :src="`images/aux-icons/${value.icon}`"
-                      alt=""
-                    />
-                    <p
-                      v-text="value.name"
-                      class="text-xs text-center text-black"
-                    ></p>
+                  <div v-for="(value, index) in item.specialItems" :key="'value' + index">
+                    <img class="w-8 h-8 m-4 mb-2" :src="`images/aux-icons/${value.icon}`" alt />
+                    <p v-text="value.name" class="text-xs text-center text-black"></p>
                   </div>
                 </template>
               </modalcards>
@@ -124,14 +93,10 @@
         </div>
         <!--English version-->
         <div v-if="eng" class="lg:w-3/4">
-          <div
-            class="opacity-entrance relative pb-8 lg:pl-20 lg:mt-32 text-white text-left"
-          >
+          <div class="opacity-entrance relative pb-8 lg:pl-20 lg:mt-32 text-white text-left">
             <p
-              class="mt-4 text-4xl text-center lg:text-left lg:font-thin lg:text-xxl lg:text-atoom-semiGray lg:pl-4  "
-            >
-              About me
-            </p>
+              class="mt-4 text-4xl text-center lg:text-left lg:font-thin lg:text-xxl lg:text-atoom-semiGray lg:pl-4"
+            >About me</p>
             <div class="lg:pr-8 lg:pl-16 lg:mt-20">
               <p class="mb-4">
                 Isaac Núñez, passionate about UX, technology and web design.
@@ -156,12 +121,8 @@
           <div class="opacity-entrance opacity-entrance--second">
             <p
               class="pb-8 text-2xl text-center text-white lg:text-left lg:text-3xl"
-            >
-              Projects I have worked on
-            </p>
-            <div
-              class="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-8 justify-items-center"
-            >
+            >Projects I have worked on</p>
+            <div class="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-8 justify-items-center">
               <modalcards
                 v-for="(item, i) in portfolio"
                 :key="i"
@@ -174,41 +135,22 @@
               >
                 <template v-slot:carousel>
                   <vue-carousel :per-page="1" :loop="true" :autoplay="true">
-                    <vue-slide
-                      v-for="(value, index) in item.galery"
-                      :key="'value' + index"
-                    >
-                      <img :src="`images/portafolio/${value.img}`" alt="" />
+                    <vue-slide v-for="(value, index) in item.galery" :key="index">
+                      <img :src="`images/portafolio/${value.img}`" alt />
                     </vue-slide>
                   </vue-carousel>
                 </template>
                 <template v-slot:tecnologies>
-                  <div
-                    v-for="(value, index) in item.tecnologies"
-                    :key="'value' + index"
-                  >
+                  <div v-for="(value, index) in item.tecnologies" :key="index">
                     <fa-icon
                       class="m-4 mb-2 text-atoom-dkOverlay"
                       :icon="['fab', `${value.icon}`]"
                     />
-                    <p
-                      v-text="value.name"
-                      class="text-xs text-center text-black"
-                    ></p>
+                    <p v-text="value.name" class="text-xs text-center text-black"></p>
                   </div>
-                  <div
-                    v-for="(value, index) in item.specialItems"
-                    :key="'value' + index"
-                  >
-                    <img
-                      class="w-8 h-8 m-4 mb-2"
-                      :src="`images/aux-icons/${value.icon}`"
-                      alt=""
-                    />
-                    <p
-                      v-text="value.name"
-                      class="text-xs text-center text-black"
-                    ></p>
+                  <div v-for="(value, index) in item.specialItems" :key="'value' + index">
+                    <img class="w-8 h-8 m-4 mb-2" :src="`images/aux-icons/${value.icon}`" alt />
+                    <p v-text="value.name" class="text-xs text-center text-black"></p>
                   </div>
                 </template>
               </modalcards>
@@ -227,24 +169,24 @@ import Modalcards from "~/components/ModalCards.vue";
 import VueCarousel from "vue-carousel/src/Carousel.vue";
 import VueSlide from "vue-carousel/src/Slide.vue";
 export default {
-  name: "AboutMe",
+  name: "Portafolio",
   asyncData() {
     return {
       portafolio,
-      portfolio
+      portfolio,
     };
   },
   components: {
     Modalcards,
     VueCarousel,
-    VueSlide
+    VueSlide,
   },
   data: () => ({
     portafolio: [],
     portfolio: [],
     esp: true,
     eng: false,
-    lang: "español"
+    lang: "español",
   }),
   methods: {
     selectEsp() {
@@ -258,7 +200,7 @@ export default {
       this.eng = true;
       this.lang = "english";
       console.log(this.lang);
-    }
-  }
+    },
+  },
 };
 </script>
